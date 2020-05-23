a9b22c6f1a756156040274b18aabd38fe5a9bae4
DenizKKaplan44 <56250103+DenizKKaplan44@users.noreply.github.com>
2020-05-23 17:25:44 +0300

    Ahmet Serkan Fidancı Gün sonu Tabloların silinmesi
    Gün sonu yani belirlediğimiz bir saatten sonra sisteme kayıt yapılamaz ve tabloların içerisini temizlenir.

--- HastaKayit/src/Servlet/SevrletHastane.java
+++ HastaKayit/src/Servlet/SevrletHastane.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,8 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import DB.kayýt;
-import Randevular.KAYITLAR;
+
 
 /**
  * Servlet implementation class SevrletHastane
@@ -46,30 +47,53 @@ public class SevrletHastane extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		
+		// Burada veritabanýna kayýt yapýlýyor ve Zaman kontrolu yapýlarak tablo temizleniyor...
+		// Saat 18:00 da kayýt yapmaya çalýþýnýlýrsa kayýt yapmaz ve tablolar temizlenir...
 	     String TCno = request.getParameter("txtTC");
 	     String ad = request.getParameter("txtAd");
 	     String tel = request.getParameter("txtTel");
 	     String connUrl= "jdbc:sqlserver://localhost:1433;databaseName=Hastane;integratedSecurity=true;";
-	     
+	 
+	     String pattern = "MMddyyyy";
+	     SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+	     String date = simpleDateFormat.format(new Date());
+	     String pattern2 = "kk";
+	     SimpleDateFormat simpleDate2Format = new SimpleDateFormat(pattern2);
+	     String saat = simpleDate2Format.format(new Date());
+	     int a = 18;
 	     try {
 	    	
 			Connection conn = DriverManager.getConnection(connUrl);
-			PreparedStatement pst2 = conn.prepareStatement("INSERT INTO KAYITLAR VALUES (?,?,?,?,?)");
+			PreparedStatement pst2 = conn.prepareStatement("INSERT INTO KAYITLAR VALUES (?,?,?,?,?,?)");
 			    pst2.setString(1, TCno);
 	            pst2.setString(2, ad);
 	            pst2.setString(3, tel);
 	            pst2.setInt(4,10);
 	            pst2.setInt(5,0);
+	            pst2.setString(6,"Bolum");
 	            
 	            pst2.execute(); 
+	            if(Integer.parseInt(saat)>a) {
+	   	         
+	   	      System.out.print("Kayýt Baþarýlý");
+	   	     }
+	   	        else
+	   	     {
+	   	         Connection conn2 = DriverManager.getConnection(connUrl);
+	   	         PreparedStatement pst3 = conn2.prepareStatement("DELETE FROM KAYITLAR");
+	   	          pst3.execute();  
+	   	       System.out.print("Bu saate sisteme kayýt Yapýlamaz");
+	   	     }
 	            
-	            System.out.print("Kayýt Baþarýlý");
+	            
 	            
 		} catch (SQLException e) {
 			
 			e.printStackTrace();
 		}
+	    
+	     
+	     
 	
 	}
 
